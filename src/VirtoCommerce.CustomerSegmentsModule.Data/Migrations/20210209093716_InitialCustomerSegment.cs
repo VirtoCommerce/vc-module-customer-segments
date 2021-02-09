b4d2ba8158a991244c6f62734127fa5ab07e7f9c
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2021-02-09 11:41:55 +0200

    #VP-6920 add initial migraiton

---
+++ src/VirtoCommerce.CustomerSegmentsModule.Data/Migrations/20210209093716_InitialCustomerSegment.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VirtoCommerce.CustomerSegmentsModule.Data.Migrations
+{
+    public partial class InitialCustomerSegment : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CustomerSegments",
+                columns: table => new
+                {
+                    Id = table.Column<string>(maxLength: 128, nullable: false),
+                    CreatedDate = table.Column<DateTime>(nullable: false),
+                    ModifiedDate = table.Column<DateTime>(nullable: true),
+                    CreatedBy = table.Column<string>(maxLength: 64, nullable: true),
+                    ModifiedBy = table.Column<string>(maxLength: 64, nullable: true),
+                    Name = table.Column<string>(maxLength: 128, nullable: false),
+                    Description = table.Column<string>(maxLength: 1024, nullable: true),
+                    UserGroup = table.Column<string>(maxLength: 64, nullable: true),
+                    IsActive = table.Column<bool>(nullable: false),
+                    StartDate = table.Column<DateTime>(nullable: true),
+                    EndDate = table.Column<DateTime>(nullable: true),
+                    ExpressionTreeSerialized = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CustomerSegments", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CustomerSegments");
+        }
+    }
+}
