07a939468dffae15be33b8d0bbff66f6bf751874
Egidijus Mazeika <em@virtoway.com>
2021-02-12 00:16:16 +0200

    VP-6286: Build preview filter

--- src/VirtoCommerce.CustomerSegmentsModule.Web/Scripts/blades/customerSegments-preview.js
+++ src/VirtoCommerce.CustomerSegmentsModule.Web/Scripts/blades/customerSegments-preview.js
@@ -1,76 +1,69 @@
 angular.module('virtoCommerce.customerSegmentsModule')
-.controller('virtoCommerce.customerSegmentsModule.customerSegmentsPreview',
-    ['$scope', 'virtoCommerce.customerModule.members',
-    'platformWebApp.bladeUtils', 'platformWebApp.uiGridHelper', 'platformWebApp.ui-grid.extension',
-    'virtoCommerce.customerModule.memberTypesResolverService', 'virtoCommerce.customerSegmentsModule.customerSearchCriteriaBuilder',
-    function($scope, membersApi, bladeUtils, uiGridHelper, gridOptionExtension, memberTypesResolverService, customerSearchCriteriaBuilder) {
-        $scope.uiGridConstants = uiGridHelper.uiGridConstants;
+    .controller('virtoCommerce.customerSegmentsModule.customerSegmentsPreview',
+        ['$scope', 'virtoCommerce.customerModule.members',
+            'platformWebApp.bladeUtils', 'platformWebApp.uiGridHelper', 'platformWebApp.ui-grid.extension',
+            'virtoCommerce.customerModule.memberTypesResolverService', 'virtoCommerce.customerSegmentsModule.customerSearchCriteriaBuilder',
+            function ($scope, membersApi, bladeUtils, uiGridHelper, gridOptionExtension, memberTypesResolverService, customerSearchCriteriaBuilder) {
+                $scope.uiGridConstants = uiGridHelper.uiGridConstants;
 
-        var blade = $scope.blade;
-        blade.title = 'customerSegments.blades.customer-segment-preview.title';
-        blade.toolbarCommands = [
-            {
-                name: "platform.commands.refresh", icon: 'fa fa-refresh',
-                executeMethod: () => blade.refresh(),
-                canExecuteMethod: () => true
-            }
-        ];
+                var blade = $scope.blade;
+                blade.headIcon = 'fa fa-eye';
+                blade.title = 'customerSegments.blades.customer-segment-preview.title';
+                blade.toolbarCommands = [
+                    {
+                        name: "platform.commands.refresh", icon: 'fa fa-refresh',
+                        executeMethod: () => blade.refresh(),
+                        canExecuteMethod: () => true
+                    }
+                ];
 
-        blade.currentEntity = {};
+                blade.refresh = () => {
+                    blade.isLoading = true;
 
-        function initializeBlade() {
-            blade.currentEntity = angular.copy(blade.originalEntity);
-        }
-
-        blade.refresh = () => {
-            blade.isLoading = true;
-
-            let searchCriteria = getSearchCriteria();
-            membersApi.search(searchCriteria, searchResult => {
-                $scope.pageSettings.totalItems = searchResult.totalCount;
-                let memberTypeDefinition;
-                _.each(searchResult.results, (x) => {
-                        if (memberTypeDefinition = memberTypesResolverService.resolve(x.memberType)) {
-                            x._memberTypeIcon = memberTypeDefinition.icon;
-                        }
+                    let searchCriteria = getSearchCriteria();
+                    membersApi.search(searchCriteria, searchResult => {
+                        $scope.pageSettings.totalItems = searchResult.totalCount;
+                        let memberTypeDefinition;
+                        _.each(searchResult.results, (x) => {
+                            if ((memberTypeDefinition = memberTypesResolverService.resolve(x.memberType)) !== null) {
+                                x._memberTypeIcon = memberTypeDefinition.icon;
+                            }
+                        });
+                        $scope.customers = searchResult.results || [];
+                        blade.isLoading = false;
                     });
-                $scope.customers = searchResult.results ? searchResult.results : [];
-                blade.isLoading = false;
-            });
-        }
+                }
 
-        var filter = $scope.filter = { };
+                var filter = $scope.filter = {};
 
-        filter.criteriaChanged = () => {
-            if ($scope.pageSettings.currentPage > 1) {
-                $scope.pageSettings.currentPage = 1;
-            } else {
-                blade.refresh();
-            }
-        };
+                filter.criteriaChanged = () => {
+                    if ($scope.pageSettings.currentPage > 1) {
+                        $scope.pageSettings.currentPage = 1;
+                    } else {
+                        blade.refresh();
+                    }
+                };
 
-        // ui-grid
-        $scope.setGridOptions = (gridId, gridOptions) => {
-            $scope.gridOptions = gridOptions;
-            gridOptionExtension.tryExtendGridOptions(gridId, gridOptions);
+                // ui-grid
+                $scope.setGridOptions = (gridId, gridOptions) => {
+                    $scope.gridOptions = gridOptions;
+                    gridOptionExtension.tryExtendGridOptions(gridId, gridOptions);
 
-            gridOptions.onRegisterApi = (gridApi) => {
-                $scope.gridApi = gridApi;
-                gridApi.core.on.sortChanged($scope, () => {
-                    if (!blade.isLoading) blade.refresh();
-                });
-            };
+                    gridOptions.onRegisterApi = (gridApi) => {
+                        $scope.gridApi = gridApi;
+                        gridApi.core.on.sortChanged($scope, () => {
+                            if (!blade.isLoading) blade.refresh();
+                        });
+                    };
 
-            bladeUtils.initializePagination($scope);
-        };
+                    bladeUtils.initializePagination($scope);
+                };
 
-        function getSearchCriteria() {
-            let searchCriteria = customerSearchCriteriaBuilder.build(filter.keyword, blade.properties);
-            searchCriteria.sort = uiGridHelper.getSortExpression($scope);
-            searchCriteria.skip = ($scope.pageSettings.currentPage - 1) * $scope.pageSettings.itemsPerPageCount;
-            searchCriteria.take = $scope.pageSettings.itemsPerPageCount
-            return searchCriteria;
-        }
-
-        initializeBlade();
-    }]);
+                function getSearchCriteria() {
+                    let searchCriteria = customerSearchCriteriaBuilder.build(filter.keyword, blade.properties);
+                    searchCriteria.sort = uiGridHelper.getSortExpression($scope);
+                    searchCriteria.skip = ($scope.pageSettings.currentPage - 1) * $scope.pageSettings.itemsPerPageCount;
+                    searchCriteria.take = $scope.pageSettings.itemsPerPageCount
+                    return searchCriteria;
+                }
+            }]);
--- src/VirtoCommerce.CustomerSegmentsModule.Web/Scripts/services/customer-searchCriteria-builder.js
+++ src/VirtoCommerce.CustomerSegmentsModule.Web/Scripts/services/customer-searchCriteria-builder.js
@@ -1,24 +1,35 @@
 angular.module('virtoCommerce.customerSegmentsModule')
-.factory('virtoCommerce.customerSegmentsModule.customerSearchCriteriaBuilder', function() {
-    return {
-        build: (keyword, properties) => {
-            let searchPhrase = [];
-            if (keyword) {
-                searchPhrase.push(keyword);
-            }
+    .factory('virtoCommerce.customerSegmentsModule.customerSearchCriteriaBuilder', function () {
+        return {
+            build: (keyword, properties) => {
+                let searchPhrase = [];
+                if (keyword) {
+                    searchPhrase.push(keyword);
+                }
 
-            if (properties) {
-                properties.forEach(property => {
-                    const values = property.values.map(value => value.value !== undefined && value.value !== null ? value.value.name || value.value : '').join('","');
-                    searchPhrase.push(`"${property.name}":"${values}"`);
-                });
-            }
+                if (properties) {
+                    properties.forEach(property => {
+                        if (property.name === 'organizations') {
+                            if (_.some(property.values)) {
+                                searchPhrase.push(`"parentorganizations":"${property.values.join('","')}"`);
+                            }
+                        } else if (property.name === 'associatedOrganizations') {
+                            if (_.some(property.values)) {
+                                 searchPhrase.push(`"associatedOrganizations":"${property.values.join('","')}"`);
+                            }
+                        } else {
+                            const values = property.values.map(value => value.value !== undefined && value.value !== null ? value.value.name || value.value : '').join('","');
+                            searchPhrase.push(`"${property.name}":"${values}"`);
+                        }
+                    });
+                }
 
-            return {
-                searchPhrase: searchPhrase.join(' '),
-                deepSearch: true,
-                objectType: 'Member'
-            };
+                return {
+                    searchPhrase: searchPhrase.join(' '),
+                    deepSearch: true,
+                    responseGroup: 'default',
+                    memberType: 'Contact'
+                };
+            }
         }
-    }
-});
+    });
